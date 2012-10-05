cad4fbcab42c111bfa198321855762c5823c6926
Raphael Fleischlin <git@raefftec.ch>
2012-10-05 15:48:25 +0200

    database schema for webshop

---
+++ enapp12-tafleisc/enapp12-tafleisc-ejb/webshop.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE `enappwebshop`;
+ 
+CREATE TABLE `enappwebshop`.`product` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45),
+  `mediapath` VARCHAR(180),
+  `unitprice` DECIMAL NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+ 
+CREATE TABLE `enappwebshop`.`customer` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(15) NOT NULL UNIQUE,
+  `password` VARCHAR(32) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(90) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+ 
+CREATE TABLE `enappwebshop`.`purchase` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `customerid` INTEGER UNSIGNED NOT NULL,
+  `datetime` DATETIME NOT NULL,
+  `status` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fkcustomer` FOREIGN KEY (`customerid`)
+    REFERENCES `enappwebshop`.`customer` (`id`)
+) ENGINE=INNODB;
+ 
+CREATE TABLE `enappwebshop`.`purchaseitem` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `purchaseid` INTEGER UNSIGNED NOT NULL,
+  `productid` INTEGER UNSIGNED NOT NULL,
+  `quantity` DECIMAL NOT NULL,
+  `unitprice` DECIMAL NOT NULL,
+  `lineamount` DECIMAL NOT NULL,
+  `description` VARCHAR(90),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fkpurchase` FOREIGN KEY (`purchaseid`)
+    REFERENCES `enappwebshop`.`purchase` (`id`),
+  CONSTRAINT `fkproduct` FOREIGN KEY (`productid`)
+    REFERENCES `enappwebshop`.`product` (`id`)
+) ENGINE=INNODB;
